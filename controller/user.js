16f5164605943c5758ef2dd922e6445faf1ce4d8
Aldric Pereira <52449719+A1dricAP@users.noreply.github.com>
2020-07-09 23:30:53 +0530

    6

---
+++ controller/user.js
@@ -0,0 +1,5 @@
+//controlling the exports from this folder
+
+exports.sayHi = (req, res) => {
+  res.json({ message: "hello there" });
+};
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const router = express.Router(); //Router() needs to be invoked
+const router = express.Router(); //Router() needs to be invoked. [Creating a router object]
 
-const name = "AP";
-router.get("/", (req, res) => {
-  res.send(`Hello from user.js ${name}`);
-});
+//getting the export function from "controller/user" directory in the project
+const { sayHi } = require("../controller/user");
+
+//using the sayHi function from the imported file. [controller/user]
+router.get("/", sayHi);
 
 module.exports = router;
