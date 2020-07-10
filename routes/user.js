6592a8cd806ad108f7aaf28e81495938aa90871a
Aldric Pereira <52449719+A1dricAP@users.noreply.github.com>
2020-07-11 01:08:04 +0530

    14

--- routes/user.js
+++ routes/user.js
@@ -6,10 +6,10 @@ this file takes care of the routing on the page. the data submitted into this pa
 const express = require("express");
 const router = express.Router(); //Router() needs to be invoked. [Creating a router object]
 
-//getting the export function from "controller/user" directory in the project
+//getting the export function from "../controller/user" directory in the project
 const { signup } = require("../controller/user");
 
-//using the sayHi function from the imported file. [controller/user]
+//using the signup function from the imported file. [../controller/user]
 router.post("/signup", signup);
 
 module.exports = router;
