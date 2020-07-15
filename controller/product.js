a448a21885109f16f48ff00497912b459d2d3f06
Aldric Pereira <52449719+A1dricAP@users.noreply.github.com>
2020-07-16 00:13:22 +0530

    21

--- controller/product.js
+++ controller/product.js
@@ -2,10 +2,26 @@ const formidable = require("formidable");
 const _ = require("lodash");
 
 const Product = require("../models/product");
-// const { parse } = require("dotenv/types");
 const fs = require("fs"); //to get access to the file system.
 const { errorHandler } = require("../helpers/dbErrorHandler");
-// const { propfind } = require("../routes/auth");
+// const product = require("../models/product");
+
+exports.productById = (req, res, next, id) => {
+  Product.findById(id).exec((err, product) => {
+    if (err || !product) {
+      return res.status(400).json({
+        error: "Product not found, mate.",
+      });
+    }
+    req.product = product; //this statement basically puts the found product with its correct corresponding id, into the req.product
+    next();
+  });
+};
+
+exports.read = (req, res) => {
+  req.product.photo = undefined;
+  return res.json(req.product);
+};
 
 exports.create = (req, res) => {
   let form = new formidable.IncomingForm();
@@ -17,9 +33,31 @@ exports.create = (req, res) => {
         error: "Error, man! Image could not be uploaded!",
       });
     }
+
+    const { name, description, price, category, quantity, shipping } = fields;
+    if (
+      !name ||
+      !description ||
+      !price ||
+      !category ||
+      !quantity ||
+      !shipping
+    ) {
+      return res.status(400).json({
+        error: "All fields are required, my man!",
+      });
+    }
+
     let product = new Product(fields);
 
     if (files.photo) {
+      if (files.photo.size > 100000) {
+        //creating this, to keep the file size in check
+        return res.status(400).json({
+          error:
+            "Error man! Image too BIGGG! Image should be less than 1mb in size.",
+        });
+      }
       product.photo.data = fs.readFileSync(files.photo.path);
       product.photo.contentType = files.photo.type;
     }
@@ -31,6 +69,21 @@ exports.create = (req, res) => {
         });
       }
       res.json(result);
+    });
+  });
+};
+
+exports.remove = (req, res) => {
+  let product = req.product;
+  product.remove((err, deletedproduct) => {
+    if (err) {
+      return res.status(400).json({
+        err: errorHandler(err),
+      });
+    }
+    res.json({
+      deletedproduct,
+      message: "product deleted successfully!",
     });
   });
 };
--- routes/product.js
+++ routes/product.js
@@ -7,8 +7,8 @@ will be sent to the controller [/controller.user]
 const express = require("express");
 const router = express.Router(); //Router() needs to be invoked. [Creating a router object]
 
-//getting the export function from "../controller/category" directory in the project
-const { create } = require("../controller/product");
+//getting the export functions from "../controller/product" directory in the project
+const { create, productById, read, remove } = require("../controller/product");
 //and "../controller/auth"
 const { requireSignin, isAuth, isAdmin } = require("../controller/auth");
 //and also "../controller/user"
@@ -17,7 +17,20 @@ const { userById } = require("../controller/user");
 //using the different functions from the imported files. [../controller/user,auth,category]
 router.post("/product/create/:userId", requireSignin, isAdmin, isAuth, create);
 
+//using the fucntion from the imported files. [../controller/product]
+router.get("/product/:productId", read);
+
+//using the functions [called middlewares] from the imported files. [../controller/product]
+router.delete(
+  "/product/:productId/:userId",
+  requireSignin,
+  isAdmin,
+  isAuth,
+  remove
+);
+
 router.param("userId", userById); //anytime there is "userId" in the route parameter, userById will run.
+router.param("productId", productById); //anytime there is "userId" in the route parameter, userById will run.
 /************************************************************************************************************************/
 
 module.exports = router;
